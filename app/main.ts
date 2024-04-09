ec9b3c8d9156976cdb7592f02918ec207ed36db7
Abdellatif Sraiti <abdellsraiti019@gmail.com>
2024-04-09 01:41:24 +0000

    refactored the redis redis replication handshake for slave part

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,6 @@
-import { log } from "node:console";
 import * as net from "node:net";
-import { parse } from "node:path";
 import RedisConnectionHandler from "./redisConnectionHandler.ts";
+import { RedisReplicationClient } from "./redisReplicationHandler.ts";
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
@@ -53,87 +52,11 @@ function instanceNatureCheck() {
   }
 }
 
-let masterPong = false;
-let masterReplconf1 = false;
-let masterReplconf2 = false;
-let masterPSYNC = false;
-
 function startHandshakeProcess(masterPort: number, masterAddress: string) {
   const client = net.createConnection(
     { port: masterPort, host: masterAddress },
     () => {
-      console.log("started Handshake Process with MASTER");
-
-      console.log("Handshake Process with MASTER 1/3");
-      console.log("PING");
-
-      if (!masterPong && !masterReplconf1 && !masterReplconf2) {
-        console.log("notice me master PING");
-
-        client.write("*1\r\n$4\r\nping\r\n");
-      }
-
-      client.on("data", (data) => {
-        const response = data.toString().trim();
-
-        console.log({
-          masterPong,
-          masterReplconf1,
-          masterReplconf2,
-          masterPSYNC,
-        });
-
-        if (response === "+PONG") {
-          masterPong = true;
-        }
-        if (response === "+OK") {
-          masterReplconf1 = true;
-        }
-        if (masterReplconf2) {
-          masterPSYNC = true;
-        }
-
-        if (response === "+OK" && masterReplconf1 && masterPong) {
-          masterReplconf2 = true;
-        }
-
-        if (response.includes("FULLRESYNC")) {
-          console.log("FULLRESYNC");
-        }
-
-        console.log({ response });
-
-        if (masterPong && !masterReplconf1 && !masterReplconf2) {
-          console.log("Handshake Process with MASTER 2/3");
-          console.log("REPLCONF");
-          client.write(
-            `*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n${instancePort}\r\n`
-          );
-          return;
-        } else if (masterReplconf2 && !masterPSYNC) {
-          console.log("Handshake Process with MASTER 3/3");
-          console.log("Replconf2  capa npsync2");
-
-          client.write(
-            `*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n`
-          );
-          return;
-        } else if (masterPSYNC) {
-          console.log("last stage :", "PSYNC ? -1");
-          console.log({ masterPSYNC });
-
-          client.write(
-            `*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n`,
-            (error) => {
-              if (error) {
-                console.error("Error writing to socket:", error);
-                // Handle the error appropriately (e.g., close the socket)
-              }
-            }
-          );
-          return;
-        }
-      });
+      new RedisReplicationClient(client, instancePort);
     }
   );
 }
---
+++ app/redisReplicationHandler.ts
@@ -0,0 +1,76 @@
+import * as net from "node:net";
+
+enum ReplicationStage {
+  PING = "PING",
+  REPLCONF = "REPLCONF",
+  REPLCONF1 = "REPLCONF1",
+  PSYNC = "PSYNC",
+}
+
+enum MasterResponses {
+  PONG = "+PONG",
+  OK = "+OK",
+}
+
+class RedisReplicationClient {
+  private replicationStage: ReplicationStage = ReplicationStage.PING;
+  private slavePort;
+
+  constructor(private connection: net.Socket, slavePort: number) {
+    console.log("initialize RedisConnectionHandler");
+    this.connection.on("data", this.handleData.bind(this));
+    this.slavePort = slavePort;
+
+    console.log("notice me master PING");
+    this.writeResponse("*1\r\n$4\r\nping\r\n");
+  }
+
+  private handleData(data: Buffer) {
+    const response = data.toString().trim();
+
+    if (!response) {
+      this.writeResponse("-ERR invalid request\r\n");
+      return;
+    }
+
+    switch (response) {
+      case MasterResponses.PONG:
+        this.replicationStage = ReplicationStage.REPLCONF;
+        this.writeResponse(
+          `*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n${this.slavePort}\r\n`
+        );
+        this.replicationStage = ReplicationStage.REPLCONF1;
+
+        break;
+
+      case MasterResponses.OK:
+        if (this.replicationStage === ReplicationStage.REPLCONF1) {
+          this.writeResponse(
+            `*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n`
+          );
+
+          this.replicationStage = ReplicationStage.PSYNC;
+          break;
+        } else if (this.replicationStage === ReplicationStage.PSYNC) {
+          this.writeResponse(`*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n`);
+          break;
+        }
+        break;
+
+      default:
+        console.log("didn't know master can do this : ", { response });
+        return;
+    }
+  }
+
+  private writeResponse(response: string) {
+    this.connection.write(response, (error) => {
+      if (error) {
+        console.error("Error writing to socket:", error);
+        // Handle the error appropriately (e.g., close the socket)
+      }
+    });
+  }
+}
+
+export { RedisReplicationClient };
